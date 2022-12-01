d4d4cac43c34d4ac5b47f07036178be3355f61a1
imwyf <1185095602@qq.com>
2022-12-01 13:29:38 +0800

    相机跟随优化

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,7 +4,9 @@ using System.Collections;
 public class CameraController : MonoBehaviour
 {
 
-    public Transform player;// 获得角色
+    public Transform player1;// 获得角色
+    public Transform player2;// 获得角色
+    Vector3 camCenter; // 相机中心点
     public Vector2 Margin;// 相机与角色的相对范围
     public Vector2 smoothing;// 相机移动的平滑度
     public BoxCollider2D Bounds;// 背景的边界
@@ -25,15 +27,16 @@ public class CameraController : MonoBehaviour
     {
         var x = transform.position.x;
         var y = transform.position.y;
+        camCenter = GetBetweenPoint(player1.position, player2.position);
         if (IsFollowing)
         {
-            if (Mathf.Abs(x - player.position.x) > Margin.x)
+            if (Mathf.Abs(x - camCenter.x) > Margin.x)
             {// 如果相机与角色的x轴距离超过了最大范围则将x平滑的移动到目标点的x
-                x = Mathf.Lerp(x, player.position.x, smoothing.x * Time.deltaTime);
+                x = Mathf.Lerp(x, camCenter.x, smoothing.x * Time.deltaTime);
             }
-            if (Mathf.Abs(y - player.position.y) > Margin.y)
+            if (Mathf.Abs(y - camCenter.y) > Margin.y)
             {// 如果相机与角色的y轴距离超过了最大范围则将x平滑的移动到目标点的y
-                y = Mathf.Lerp(y, player.position.y, smoothing.y * Time.deltaTime);
+                y = Mathf.Lerp(y, camCenter.y, smoothing.y * Time.deltaTime);
             }
         }
         float orthographicSize = GetComponent<Camera>().orthographicSize;// orthographicSize代表相机(或者称为游戏视窗)竖直方向一半的范围大小,且不随屏幕分辨率变化(水平方向会变)
@@ -41,5 +44,19 @@ public class CameraController : MonoBehaviour
         x = Mathf.Clamp(x, _min.x + cameraHalfWidth, _max.x - cameraHalfWidth);// 限定x值
         y = Mathf.Clamp(y, _min.y + orthographicSize, _max.y - orthographicSize);// 限定y值
         transform.position = new Vector3(x, y, transform.position.z);// 改变相机的位置
+    }
+
+    /// <summary>
+    /// 获取两点之间距离一定百分比的一个点
+    /// </summary>
+    /// <param name="start">起始点</param>
+    /// <param name="end">结束点</param>
+    /// <param name="distance">起始点到目标点距离百分比</param>
+    /// <returns></returns>
+    private Vector3 GetBetweenPoint(Vector3 start, Vector3 end, float percent = 0.5f)
+    {
+        Vector3 normal = (end - start).normalized;
+        float distance = Vector3.Distance(start, end);
+        return normal * (distance * percent) + start;
     }
 }
